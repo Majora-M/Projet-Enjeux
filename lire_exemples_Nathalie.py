54dcc12994420d09514556846b7ea9006d18a876
Majora-M <33903192+Majora-M@users.noreply.github.com>
2017-11-22 16:21:50 +0100

    Update lire_exemples_Nathalie.py

--- lire_exemples_Nathalie.py
+++ lire_exemples_Nathalie.py
@@ -14,35 +14,35 @@ def extract_percentage(string):
 def read_file(file_name):
     file=open(file_name, "r")
     i=0
-    sets=[[[],[],[],[],[],[],[],[]]]
+    sets=[[]]
     for line in file:
         if str(line)[0]=="\n":
-            sets.append([[],[],[],[],[],[],[],[]])
+            sets.append([])
             i+=1
-        if str(line)[0]=="i" : sets[i][0]=int(''.join(ele for ele in line if ele.isdigit()))
-        if str(line)[0]=="l" : sets[i][1]=1
+        if str(line)[0]=="i" : sets[i].append(int(''.join(ele for ele in line if ele.isdigit())))
+        if str(line)[0]=="l" : sets[i].append(1)
         # originalement : le label str(line)[8:-1]
         if str(line)[0]=="*" :
-            if str(line)[1]=="C" and str(line)[2]==" " : sets[i][2]=extract_percentage(str(line))
-            if str(line)[1]=="C" and str(line)[2]=="r" : sets[i][3]=extract_percentage(str(line))
-            if str(line)[1]=="N" and str(line)[2]==" " : sets[i][4]=extract_percentage(str(line))
-            if str(line)[1]=="N" and str(line)[2]=="a" : sets[i][5]=extract_percentage(str(line))
-            if str(line)[1]=="O"                       : sets[i][6]=extract_percentage(str(line))
-            if str(line)[1]=="S"                       : sets[i][7]=extract_percentage(str(line))
-    return sets
+            if str(line)[1]=="C" and str(line)[2]==" " : sets[i].append(extract_percentage(str(line)))
+            if str(line)[1]=="C" and str(line)[2]=="r" : sets[i].append(extract_percentage(str(line)))
+            if str(line)[1]=="N" and str(line)[2]==" " : sets[i].append(extract_percentage(str(line)))
+            if str(line)[1]=="N" and str(line)[2]=="a" : sets[i].append(extract_percentage(str(line)))
+            if str(line)[1]=="O"                       : sets[i].append(extract_percentage(str(line)))
+            if str(line)[1]=="S"                       : sets[i].append(extract_percentage(str(line)))
+    return sets[0:130]
 
 def get_sets(nb_files):
     Sets=[]
     for i in range(1, nb_files+1):
-        Sets.append(read_file("donnees/Jeu drugs and explosives_15 "+str(i)+".txt"))
+        Sets=Sets+read_file("donnees/Jeu drugs and explosives_15 "+str(i)+".txt")
         print("Set", i, "is of length", len(Sets[i-1]))
     return Sets
 
 def main():
     # ici : n=6
-    nb_files=10
+    nb_files=2
     Sets=get_sets(nb_files)
-    print(Sets[0][:3])
+    print(Sets)
     
 main()
 
