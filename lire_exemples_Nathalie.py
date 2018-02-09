7d793824d3fbc7302defcfc846b7faf920fd420a
QuentinBlampey <33903498+QuentinBlampey@users.noreply.github.com>
2018-02-09 11:59:31 +0100

    Update lire_exemples_Nathalie.py

--- lire_exemples_Nathalie.py
+++ lire_exemples_Nathalie.py
@@ -1,19 +1,6 @@
 import os
 import inspect
 
-"""
-## Notes
-
-# Avant d'exécuter : vérifier que "lire_exemples_Nathalie.py" et le dossier
-# "donnees" sont enregistres au meme endroit (ils doivent être à la même hauteur
-# dans l'arborescence des fichiers
-
-# Executer : dans pyzo, utiliser ctrl+shift+E ( sinon, dans le menu Run ->
-# Run file as script )
-# Le but est que le shell se place dans le dossier ou se trouvent
-# "lire_exemples_Nathalie.py" et le dossier "donnees", pour pouvoir les lire
-# correctement
-"""
 def extract_percentage(string):
     ext=""
     for i in range(len(string)):
@@ -47,27 +34,26 @@ def read_file(file_name):
 def read_file2(file_name):
     file=open(file_name, "r")
     i=0
-    sets=[[]]
+    sets=[[0 for i in range(8)]]
     for line in file:
         if str(line)[0]=="\n":
-            sets.append([])
+            sets.append([0 for i in range(8)])
             i+=1
-        if str(line)[0]=="i" : sets[i].append(int(''.join(ele for ele in line if ele.isdigit())))
-        if str(line)[0]=="l" :
-            if i <=59:
-                sets[i].append(0)
-            elif i <=144:
-                sets[i].append(1)
-            else:
-                sets[i].append(2)
+        elif str(line)[0]=="i" : sets[i][0]=int(''.join(ele for ele in line if ele.isdigit()))
+        elif str(line)[0]=="l" :
+            if i>=60:
+                if i <=144:
+                    sets[i][1]=1
+                else:
+                    sets[i][1]=2
         # originalement : le label str(line)[8:-1]
-        if str(line)[0]=="*" :
-            if str(line)[1]=="C" and str(line)[2]==" " : sets[i].append(extract_percentage(str(line)))
-            if str(line)[1]=="C" and str(line)[2]=="r" : sets[i].append(extract_percentage(str(line)))
-            if str(line)[1]=="N" and str(line)[2]==" " : sets[i].append(extract_percentage(str(line)))
-            if str(line)[1]=="N" and str(line)[2]=="a" : sets[i].append(extract_percentage(str(line)))
-            if str(line)[1]=="O"                       : sets[i].append(extract_percentage(str(line)))
-            if str(line)[1]=="S"                       : sets[i].append(extract_percentage(str(line)))
+        elif str(line)[0]=="*" :
+            if str(line)[1]=="C" and str(line)[2]==" "   : sets[i][2]=extract_percentage(str(line))
+            elif str(line)[1]=="C" and str(line)[2]=="r" : sets[i][3]=extract_percentage(str(line))
+            elif str(line)[1]=="N" and str(line)[2]==" " : sets[i][4]=extract_percentage(str(line))
+            elif str(line)[1]=="N" and str(line)[2]=="a" : sets[i][5]=extract_percentage(str(line))
+            elif str(line)[1]=="O"                       : sets[i][6]=extract_percentage(str(line))
+            else                                         : sets[i][7]=extract_percentage(str(line))
     return sets[0:190]
 
 def read_file3(file_name):
@@ -116,5 +102,3 @@ def get_sets3(nb_files):
     for i in range(1, nb_files+1):
         Sets=Sets+read_file3("donnees/Jeu drugs, explosives, benigns_simple_15 "+str(i)+".txt")
     return Sets
-
-
